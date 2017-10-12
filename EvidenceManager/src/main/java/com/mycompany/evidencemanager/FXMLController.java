76e02a0685de1712175d008444590d12ac7aefe1
Kasper Christensen <Kasper@Kaspers-MacBook-Air.local>
2017-10-12 15:39:25 +0200

    Created Login screen with zero functionality

--- EvidenceManager/src/main/java/com/mycompany/evidencemanager/FXMLController.java
+++ EvidenceManager/src/main/java/com/mycompany/evidencemanager/FXMLController.java
@@ -5,21 +5,28 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Button loginBTN;
+    @FXML
+    private TextField userNameTF;
+    @FXML
+    private TextField passwordTF;
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleLoginAction(ActionEvent event) {
+        
+    }
 }
