8959bf32da634c8bab6b262f2df2ba30ad3854f5
Øllebrød <3>
2017-10-27 13:18:43 +0200

    hej

--- EvidenceManager/EvidenceManagerServer/src/main/java/io/swagger/api/impl/EvidenceApiServiceImpl.java
+++ EvidenceManager/EvidenceManagerServer/src/main/java/io/swagger/api/impl/EvidenceApiServiceImpl.java
@@ -1,17 +1,8 @@
 package io.swagger.api.impl;
 
-import dbConnection.sqlStatement;
 import io.swagger.api.*;
-import io.swagger.model.*;
-
-import io.swagger.model.Evidence;
-
-import java.util.List;
 import io.swagger.api.NotFoundException;
 
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -19,7 +10,7 @@ import javax.ws.rs.core.SecurityContext;
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-10-26T09:35:04.468Z")
 public class EvidenceApiServiceImpl extends EvidenceApiService {
     
-    private sqlStatement i;
+    private IsqlStatement i;
     
     @Override
     public Response findEvidence(String keyWord, SecurityContext securityContext) throws NotFoundException {
