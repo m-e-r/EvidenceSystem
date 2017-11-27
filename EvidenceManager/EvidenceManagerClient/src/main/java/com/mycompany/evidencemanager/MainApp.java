d7eb0e1ed22b2f14ab0f223e938302c75dfcb384
Øllebrød <3>
2017-11-27 13:23:54 +0100

    no message

--- EvidenceManager/EvidenceManagerClient/src/main/java/com/mycompany/evidencemanager/MainApp.java
+++ EvidenceManager/EvidenceManagerClient/src/main/java/com/mycompany/evidencemanager/MainApp.java
@@ -12,11 +12,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-<<<<<<< HEAD
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginScreen.fxml"));
-=======
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ValidateUsers.fxml"));
->>>>>>> createUser
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
