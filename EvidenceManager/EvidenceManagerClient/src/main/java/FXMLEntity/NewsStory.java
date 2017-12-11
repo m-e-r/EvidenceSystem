cdd2e1bc75918333b4636208fff931ed266a1bf5
Unknown <jacos16@student.sdu.dk>
2017-12-11 14:40:01 +0100

    List of cases show right cases

--- EvidenceManager/EvidenceManagerClient/src/main/java/FXMLEntity/NewsStory.java
+++ EvidenceManager/EvidenceManagerClient/src/main/java/FXMLEntity/NewsStory.java
@@ -16,7 +16,7 @@ public enum NewsStory {
     STORY1("Police Dog On Fire:In a tragic event involving a police dog and a fire, a police dog accidently caught on fire, after a local officer threw a tennis ball into a fire in an attempt to put it out. \"They should really just let us do our job, and stick to throwing balls elsewhere\" - says head of fire department, Michael Fireman."),
     STORY2("New Chairs:The local department will recieve new chairs in the coming year. Expect to be asked about preferences in an email later this month."),
     STORY3("Woman Saves Officer:An officer was saved from embarrasment last tuesday, after a woman who wishes to remain anonymous asked him to close his zipper at the start of an 8 hour patrol shift."),
-    STORY4("Sandwich Eater Found!:The person who has been eating bites of sandwiches left in the fridge has been found and punished accorindgly.");
+    STORY4("Sandwich Eater Found!:The person who has been eating bites of sandwiches left in the fridge has been found and punished accordingly.");
     
     private String story;
     
